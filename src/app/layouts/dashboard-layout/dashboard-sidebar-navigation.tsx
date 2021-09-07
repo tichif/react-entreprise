9450dc9227eb4a46486c2be049ffde20e43168cb
Dalzon Charles-Hébert <charlby5@gmail.com>
2021-09-06 21:52:08 -0400

    Add lazy loading for splitting code

--- src/app/layouts/dashboard-layout/dashboard-sidebar-navigation.tsx
+++ src/app/layouts/dashboard-layout/dashboard-sidebar-navigation.tsx
@@ -1,5 +1,14 @@
 import React, { useEffect } from 'react';
-import { makeStyles, createStyles } from '@material-ui/core';
+import {
+  makeStyles,
+  createStyles,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+} from '@material-ui/core';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import SettingsIcon from '@material-ui/icons/Settings';
 import { Link } from 'react-router-dom';
 import Drawer from '@material-ui/core/Drawer';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -61,6 +70,26 @@ const DashboardSidebarNavigation = () => {
             Logo
           </Link>
         </Toolbar>
+        <div className={classes.drawerContainer}>
+          <List>
+            <Link className={classes.link} to={`${url}/settings-and-privacy`}>
+              <ListItem button>
+                <ListItemIcon>
+                  <SettingsIcon />
+                </ListItemIcon>
+                <ListItemText primary={'settings and privacy'} />
+              </ListItem>
+            </Link>
+            <a className={classes.link} href={'/'}>
+              <ListItem button>
+                <ListItemIcon>
+                  <ExitToAppIcon />
+                </ListItemIcon>
+                <ListItemText primary={'logout'} />
+              </ListItem>
+            </a>
+          </List>
+        </div>
       </Drawer>
     </div>
   );
--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,40 +1,52 @@
 import React, { lazy, Suspense } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import { LinearProgress } from '@material-ui/core';
 
 import Home from './views/pages/Home';
-import About from './views/pages/About';
+// import About from './views/pages/About';
 import NotFoundPage from './pages/NotFoundPage';
 import Dashboard from './layouts/dashboard-layout';
-import DashboardDefaultContent from './views/dashboard/dashboard-default-content';
-import SettingsAndPrivacy from './views/dashboard/settings-and-privacy';
+// import DashboardDefaultContent from './views/dashboard/dashboard-default-content';
+// import SettingsAndPrivacy from './views/dashboard/settings-and-privacy';
 
 const Routes = () => {
   return (
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/about" component={About} />
-      <Route exact path="/not-found" component={NotFoundPage} />
-      <Route
-        path={`/dashboard`}
-        render={({ match: { path } }) => (
-          <Dashboard>
-            <Switch>
-              <Route
-                exact
-                path={path + '/'}
-                component={DashboardDefaultContent}
-              />
-              <Route
-                exact
-                path={path + '/settings-and-privacy'}
-                component={SettingsAndPrivacy}
-              />
-            </Switch>
-          </Dashboard>
-        )}
-      />
-      <Redirect from={'*'} to="not-found" exact />
-    </Switch>
+    <Suspense fallback={<LinearProgress style={{ margin: '10rem' }} />}>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        {/* <Route exact path="/about" component={About} /> */}
+        <Route
+          exact
+          path="/about"
+          component={lazy(() => import('./views/pages/About'))}
+        />
+        <Route exact path="/not-found" component={NotFoundPage} />
+        <Route
+          path={`/dashboard`}
+          render={({ match: { path } }) => (
+            <Dashboard>
+              <Switch>
+                <Route
+                  exact
+                  path={path + '/'}
+                  component={lazy(
+                    () => import('./views/dashboard/dashboard-default-content'),
+                  )}
+                />
+                <Route
+                  exact
+                  path={path + '/settings-and-privacy'}
+                  component={lazy(
+                    () => import('./views/dashboard/settings-and-privacy'),
+                  )}
+                />
+              </Switch>
+            </Dashboard>
+          )}
+        />
+        <Redirect from={'*'} to="not-found" exact />
+      </Switch>
+    </Suspense>
   );
 };
 
