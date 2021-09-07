26abe6c2407a649b34e21669d459455ef905e082
Dalzon Charles-Hébert <charlby5@gmail.com>
2021-09-06 21:14:51 -0400

    Add different routes for the Dashboard

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -4,6 +4,9 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import Home from './views/pages/Home';
 import About from './views/pages/About';
 import NotFoundPage from './pages/NotFoundPage';
+import Dashboard from './layouts/dashboard-layout';
+import DashboardDefaultContent from './views/dashboard/dashboard-default-content';
+import SettingsAndPrivacy from './views/dashboard/settings-and-privacy';
 
 const Routes = () => {
   return (
@@ -11,6 +14,25 @@ const Routes = () => {
       <Route exact path="/" component={Home} />
       <Route exact path="/about" component={About} />
       <Route exact path="/not-found" component={NotFoundPage} />
+      <Route
+        path={`/dashboard`}
+        render={({ match: { path } }) => (
+          <Dashboard>
+            <Switch>
+              <Route
+                exact
+                path={path + '/'}
+                component={DashboardDefaultContent}
+              />
+              <Route
+                exact
+                path={path + '/settings-and-privacy'}
+                component={SettingsAndPrivacy}
+              />
+            </Switch>
+          </Dashboard>
+        )}
+      />
       <Redirect from={'*'} to="not-found" exact />
     </Switch>
   );
---
+++ src/app/views/dashboard/settings-and-privacy.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SettingsAndPrivacy = () => {
+  return (
+    <div>
+      <h1>Setting and Privacy</h1>
+    </div>
+  );
+};
+
+export default SettingsAndPrivacy;
