9489b9aa6c1731077f3bbb9afd72ed0a2f96e0aa
Dalzon Charles-Hébert <charlby5@gmail.com>
2021-10-05 13:18:37 -0400

    Create a product service
    - Modify the json database
    - Update the ProductListView

--- src/app/views/dashboard/product/ProductListView/index.tsx
+++ src/app/views/dashboard/product/ProductListView/index.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { makeStyles, Container } from '@material-ui/core';
 import { createStyles } from '@material-ui/styles';
 
 import Header from './Header';
 import Results from './Results';
+import { getProductsAxios } from 'services/productService';
+import { ProductType } from 'models/product-type';
 
 const useStyles = makeStyles(theme =>
   createStyles({
@@ -20,6 +22,31 @@ const useStyles = makeStyles(theme =>
 );
 
 const ProductListView = () => {
+  const [products, setProducts] = useState<ProductType[]>([]);
+  const [open, setOpen] = useState(false);
+
+  const fetchProducts = async () => {
+    handleToggle();
+    try {
+      const { data } = await getProductsAxios();
+      setProducts(data);
+    } catch (error) {
+      alert('Something went wrong');
+    }
+    handleClose();
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleToggle = () => {
+    setOpen(!open);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   const classes = useStyles();
   return (
     <Container>
