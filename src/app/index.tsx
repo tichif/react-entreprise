85c422ed2cbe16bcbee11c0ff768684e8cb186a2
Charles-Hebert Dalzon <charlby5@gmail.com>
2022-04-05 14:34:59 -0400

    Set up the calendar library

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -17,6 +17,10 @@ import { useTranslation } from 'react-i18next';
 // Notifications
 import { SnackbarProvider } from 'notistack';
 
+// Date Provider
+import MomentUtils from '@date-io/moment';
+import { MuiPickersUtilsProvider } from '@material-ui/pickers';
+
 import Routes from './routes';
 import MainLayout from './layouts/main-layout';
 
@@ -24,19 +28,24 @@ export function App() {
   const { i18n } = useTranslation();
   return (
     <BrowserRouter>
-      <SnackbarProvider dense maxSnack={3}>
-        <Helmet
-          titleTemplate="%s - React Boilerplate"
-          defaultTitle="React Boilerplate"
-          htmlAttributes={{ lang: i18n.language }}
-        >
-          <meta name="description" content="A React Boilerplate application" />
-        </Helmet>
-        <MainLayout>
-          <Routes />
-        </MainLayout>
-        <GlobalStyle />
-      </SnackbarProvider>
+      <MuiPickersUtilsProvider utils={MomentUtils}>
+        <SnackbarProvider dense maxSnack={3}>
+          <Helmet
+            titleTemplate="%s - React Boilerplate"
+            defaultTitle="React Boilerplate"
+            htmlAttributes={{ lang: i18n.language }}
+          >
+            <meta
+              name="description"
+              content="A React Boilerplate application"
+            />
+          </Helmet>
+          <MainLayout>
+            <Routes />
+          </MainLayout>
+          <GlobalStyle />
+        </SnackbarProvider>
+      </MuiPickersUtilsProvider>
     </BrowserRouter>
   );
 }
