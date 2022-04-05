3807c184cf82d8bbc475dd60a28b514353237bd5
Charles-Hebert Dalzon <charlby5@gmail.com>
2022-04-05 13:09:39 -0400

    Setup Redux ToolKit

--- src/index.tsx
+++ src/index.tsx
@@ -1,54 +1,42 @@
-/**
- * index.tsx
- *
- * This is the entry file for the application, only setup and boilerplate
- * code.
- */
-
 import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
-
+import 'react-quill/dist/quill.snow.css';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-
-import 'react-quill/dist/quill.snow.css';
-
-// Use consistent styling
 import 'sanitize.css/sanitize.css';
-
 // Import root app
 import { App } from 'app';
-
 import { HelmetProvider } from 'react-helmet-async';
-
 import { configureAppStore } from 'store/configureStore';
-
-import reportWebVitals from 'reportWebVitals';
-
 // Initialize languages
 import './locales/i18n';
-
 const store = configureAppStore();
 const MOUNT_NODE = document.getElementById('root') as HTMLElement;
+interface Props {
+  Component: typeof App;
+}
+/* wrapping the root component inside a provider gives all the component an
+access to the provider component or the whole application */
 
-ReactDOM.render(
+const ConnectedApp = ({ Component }: Props) => (
   <Provider store={store}>
     <HelmetProvider>
-      <App />
+      <Component />
     </HelmetProvider>
-  </Provider>,
-  MOUNT_NODE,
+  </Provider>
 );
-
-// Hot reloadable translation json files
+const render = (Component: typeof App) => {
+  ReactDOM.render(<ConnectedApp Component={Component} />, MOUNT_NODE);
+};
 if (module.hot) {
-  module.hot.accept(['./locales/i18n'], () => {
-    // No need to render the App again because i18next works with the hooks
+  // Hot reloadable translation json files and app
+  // modules.hot.accept does not accept dynamic dependencies,
+  // have to be constants at compile-time
+  module.hot.accept(['./app', './locales/i18n'], () => {
+    ReactDOM.unmountComponentAtNode(MOUNT_NODE);
+    const App = require('./app').App;
+    render(App);
   });
 }
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+render(App);
