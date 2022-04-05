3579bd7956dc5e5713ee8b42a49d90af9e9184a7
Charles-Hebert Dalzon <charlby5@gmail.com>
2022-04-05 16:13:17 -0400

    Create an Header component for the calendar page

--- src/app/views/dashboard/calendar/CalendarView/index.tsx
+++ src/app/views/dashboard/calendar/CalendarView/index.tsx
@@ -2,9 +2,22 @@ import React, { useEffect } from 'react';
 import { getEvents } from 'features/calendar/calendarSlice';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from 'store/reducers';
+import { Container, makeStyles } from '@material-ui/core';
+
+import Page from 'app/components/page';
+import Header from './Header';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    minHeight: '100%',
+    paddingTop: theme.spacing(3),
+    paddingBottom: theme.spacing(3),
+  },
+}));
 
 const Index = () => {
   const dispatch = useDispatch();
+  const classes = useStyles();
 
   useEffect(() => {
     dispatch(getEvents());
@@ -15,17 +28,16 @@ const Index = () => {
   );
 
   return (
-    <>
-      <h1>Calendar Works !!!</h1>
-      {loading && <h2>Loading....</h2>}
-      {error && <h2>Something went wrong</h2>}
+    <Page className={classes.root} title="Calendar">
+      <Container maxWidth={false}>
+        <Header />
+        <h1>Calendar Works !!!</h1>
+        {loading && <h2>Loading....</h2>}
+        {error && <h2>Something went wrong</h2>}
 
-      <ul>
-        {events?.map(e => (
-          <li key={e.id}>{e.title}</li>
-        ))}
-      </ul>
-    </>
+        <ul>{!loading && events?.map(e => <li key={e.id}>{e.title}</li>)}</ul>
+      </Container>
+    </Page>
   );
 };
 
